58dbe16ff1f1caa4706a87ae0c0a47c659ec6ca1
stefanopaesani <stefano.paesani@bristol.ac.uk>
2020-11-09 10:14:11 +0000

    Separated pure graph functions in separate file

--- GraphStateClass.py
+++ GraphStateClass.py
@@ -1,15 +1,11 @@
 ## IMPORTS ##
 
-# Used only in benchmarking
-import time
-
-import numpy as np
-from itertools import product, combinations
 import networkx as nx
-# TODO: use graph-tool instead of networkx...much faster for graphs (and better graphics), but requires Mac/Linux
+# TODO: use graph-tool instead of networkx...much faster for graphs (and better graphics), but only for Mac/Linux
 
 import cirq
 # See comments in GraphState.graph_vecstate to see why/if cirq is required
+# TODO: avoid CIRQ once the vector calculation from Griffiths is implemented
 
 import qecc as q
 
@@ -21,6 +17,7 @@ from StabStateClass import StabState
 
 
 ## CLASSES ##
+
 
 class GraphState(object):
     r"""
@@ -68,7 +65,6 @@ class GraphState(object):
         Converts the graph state into a Stablizer state.
         """
         return StabState(self.stab_gens)
-
 
     def graph_vecstate(self):
         """
@@ -143,55 +139,12 @@ def stabilizer_generators_from_graph(graph):
     return stab_gens
 
 
-## GRAPH INITIALIZATION FUNCTIONS ##
-
-def gen_linear_graph(nqubits):
-    graph = nx.Graph()
-    graph.add_nodes_from(range(nqubits))
-    these_edges = [(node_ix, node_ix + 1) for node_ix in range(nqubits - 1)]
-    graph.add_edges_from(these_edges)
-    return graph
-
-
-def gen_ring_graph(nqubits):
-    graph = nx.Graph()
-    graph.add_nodes_from(range(nqubits))
-    these_edges = [(node_ix, (node_ix + 1) % nqubits) for node_ix in range(nqubits)]
-    graph.add_edges_from(these_edges)
-    return graph
-
-
-def gen_star_graph(nqubits, central_qubit=0):
-    graph = nx.Graph()
-    nodes = range(nqubits)
-    graph.add_nodes_from(nodes)
-    graph.add_edges_from(
-        product([central_qubit], [other_nodes for other_nodes in nodes if other_nodes != central_qubit]))
-    return graph
-
-
-def gen_fullyconnected_graph(nqubits):
-    graph = nx.Graph()
-    nodes = range(nqubits)
-    graph.add_nodes_from(nodes)
-    graph.add_edges_from(combinations(nodes, 2))
-    return graph
-
-
-def gen_crazy_graph(nrows, nlayers):
-    graph = nx.Graph()
-    nodes_mat = np.arange(nrows * nlayers).reshape((nlayers, nrows))
-    graph.add_nodes_from(range(nrows * nlayers))
-    for layer_ix in range(nlayers - 1):
-        these_edges = product(nodes_mat[layer_ix], nodes_mat[layer_ix + 1])
-        graph.add_edges_from(these_edges)
-    return graph
-
-
 if __name__ == '__main__':
     ## Additional useful functions and classes
     from local_transformations import local_cliffords_on_stab_list
     from StabStateClass import StabState
+    import time
+    from graphs import gen_linear_graph, gen_star_graph, gen_fullyconnected_graph, gen_ring_graph, gen_crazy_graph
 
     ########### DEFINE GRAPHS TO USE
 
---
+++ graphs.py
@@ -0,0 +1,50 @@
+import networkx as nx
+# TODO: use graph-tool instead of networkx...much faster for graphs (and better graphics), but only for Mac/Linux
+
+import numpy as np
+from itertools import product, combinations
+
+
+## GRAPH INITIALIZATION FUNCTIONS ##
+
+def gen_linear_graph(nqubits):
+    graph = nx.Graph()
+    graph.add_nodes_from(range(nqubits))
+    these_edges = [(node_ix, node_ix + 1) for node_ix in range(nqubits - 1)]
+    graph.add_edges_from(these_edges)
+    return graph
+
+
+def gen_ring_graph(nqubits):
+    graph = nx.Graph()
+    graph.add_nodes_from(range(nqubits))
+    these_edges = [(node_ix, (node_ix + 1) % nqubits) for node_ix in range(nqubits)]
+    graph.add_edges_from(these_edges)
+    return graph
+
+
+def gen_star_graph(nqubits, central_qubit=0):
+    graph = nx.Graph()
+    nodes = range(nqubits)
+    graph.add_nodes_from(nodes)
+    graph.add_edges_from(
+        product([central_qubit], [other_nodes for other_nodes in nodes if other_nodes != central_qubit]))
+    return graph
+
+
+def gen_fullyconnected_graph(nqubits):
+    graph = nx.Graph()
+    nodes = range(nqubits)
+    graph.add_nodes_from(nodes)
+    graph.add_edges_from(combinations(nodes, 2))
+    return graph
+
+
+def gen_crazy_graph(nrows, nlayers):
+    graph = nx.Graph()
+    nodes_mat = np.arange(nrows * nlayers).reshape((nlayers, nrows))
+    graph.add_nodes_from(range(nrows * nlayers))
+    for layer_ix in range(nlayers - 1):
+        these_edges = product(nodes_mat[layer_ix], nodes_mat[layer_ix + 1])
+        graph.add_edges_from(these_edges)
+    return graph
